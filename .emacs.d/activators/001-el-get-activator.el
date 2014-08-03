1e43f791621999836828a9cc20a7d628b7c58ebb
Christian Köstlin <christian.koestlin@gmail.com>
2014-08-03 04:41:17 +0200

    some flychecking

--- .emacs.d/activators/001-el-get-activator.el
+++ .emacs.d/activators/001-el-get-activator.el
@@ -1,3 +1,6 @@
+;;; el-get-activator ---- my el-get config
+;;; Commentary:
+;;; Code:
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
@@ -6,52 +9,46 @@
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
+
+(require 'el-get-recipes)
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (setq el-get-user-package-directory "~/.emacs.d/el-get-user/init")
-(setq el-get-sources
-      '(
-        ;; base
-        el-get
-        color-theme-zenburn
-        helm
+(let ((el-get-sources
+       '(
+         ;; base
+         el-get
+         color-theme-zenburn
+         helm
 
-        ;; editor features
-        hungry-delete
-        key-chord
-        powerline
-        highlight-parentheses
-        yasnippet
-        undo-tree
-        expand-region
-        tff
-        flycheck
-        projectile
-        multiple-cursors
-        dired+
+         ;; editor features
+         hungry-delete
+         key-chord
+         powerline
+         highlight-parentheses
+         yasnippet
+         undo-tree
+         expand-region
+         tff
+         flycheck
+         projectile
+         multiple-cursors
+         dired+
 
-        ;; bigger ones
-        magit
-        git-timemachine
-        org-mode
+         ;; bigger ones
+         magit
+         git-timemachine
+         org-mode
 
-        ;; coding
-        ert-expectations
-        paredit
-        s
-        dash
-                                        ; json
-        ;; not needed
-        ;; smooth-scrolling
-        ;; auto-complete
-        ;; calfw
-        ;; howm
-        ;; emacs-soap-client
-        ;; org-jira
-        ;; xcscope
-        ;; smartparens
-        ;; ace-jump-mode
-        ;; org-reveal
-        ))
+         ;; coding
+         ert-expectations
+         paredit
+         s
+         dash
+         company-mode
+         )))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync el-get-sources)
+      (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+      (el-get 'sync el-get-sources))
+
+(provide '001-el-get-activator)
+;;; 001-el-get-activator.el ends here
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,38 @@
-(require 'cl) ; a rare necessary use of REQUIRE
-(defvar *emacs-load-start* (current-time))
-(prefer-coding-system 'utf-8)
-(setq emacs-d-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+;;; init --- My Init File
+;;; Commentary:
+;;; Code:
 
-(setq activators-dir (concat emacs-d-dir "activators/"))
+(eval-when-compile (require 'cl))
+
+(defun format-duration (time)
+  "Formats TIME as seconds and milliseconds."
+  (format "%d.%ds" (car time) (cdr time)))
 
 (defun time-diff (t1 t2)
+  "Small helper to calc the time difference between T1 and T2."
   (let* (
-	 (help-ms (- (third t1) (third t2)))
-	 (ms (if (< help-ms 0) (+ 1000000 help-ms) help-ms))
-	 (help-s (- (+ (first t1) (second t1)) (+ (first t2) (second t2))))
-	 (s (if (= help-ms ms) help-s (- help-s 1)))
-	 )
+         (help-ms (- (third t1) (third t2)))
+         (ms (if (< help-ms 0) (+ 1000000 help-ms) help-ms))
+         (help-s (- (+ (first t1) (second t1)) (+ (first t2) (second t2))))
+         (s (if (= help-ms ms) help-s (- help-s 1))))
     (cons s ms)))
 
-(dolist (activator (directory-files activators-dir t ".*-activator.el$"))
-  (message "loading %s" activator)
-  (setq start-of-package (current-time))
-  (load-library activator)
-  (setq load-time-of-package (time-diff (current-time) start-of-package))
-  (message "%s loaded in %d.%ds" activator (car load-time-of-package) (cdr load-time-of-package))
-  )
+(prefer-coding-system 'utf-8)
+(defvar emacs-d-dir (file-name-directory
+                     (or (buffer-file-name) load-file-name)) "The path to the EMACS config directory.")
+(let ((emacs-load-start (current-time)))
+  (let ((activators-dir (concat emacs-d-dir "activators/")))
+    (dolist (activator (directory-files activators-dir t ".*-activator.el$"))
+      (let* ((m1 (message "loading %s" activator))
+             (start-of-package (current-time))
+             (ok (load-library activator))
+             (load-time-of-package (time-diff (current-time) start-of-package))
+             (m2 (message "%s loaded in %s" activator (format-duration load-time-of-package)))))))
+  (let ((since-start (time-diff (current-time) emacs-load-start)))
+    (message "My .emacs loaded in %s" (format-duration since-start))))
 
-(let* (
-       (since-start (time-diff (current-time) *emacs-load-start*))
-       )
-  (message "My .emacs loaded in %d.%ds" (car since-start) (cdr since-start))
-)
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(provide 'init)
+;;; init.el ends here
+
