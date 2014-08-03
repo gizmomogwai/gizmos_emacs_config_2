ae76beb282974e21ec1a790dc9c99446cdeb72ab
Christian Köstlin <christian.koestlin@gmail.com>
2014-08-03 12:46:29 +0200

    fixed flycheck warnings

--- .emacs.d/activators/901-snippets-activator.el
+++ .emacs.d/activators/901-snippets-activator.el
@@ -1,19 +1,18 @@
 ;;; 901-snippets-activator.el --- Misc lisp snippets that are not libs
 ;;; Commentary:
 ;;; Code:
+(defun replace-in-buffer (REGEXP TO-STRING)
+  "Replace a REGEXP with TO-STRING in the whole buffer."
+  (save-excursion
+  (goto-char (point-min))
+  (while (re-search-forward REGEXP nil t)
+    (replace-match TO-STRING nil nil))))
+
 (defun umlaute-2-html ()
   "Replace umlaute with html code."
   (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (replace-regexp "ä" "&auml;"))
-  (save-excursion
-    (goto-char (point-min))
-    (replace-regexp "ö" "&ouml;"))
-  (save-excursion
-    (goto-char (point-min))
-    (replace-regexp "ü" "&uuml;"))
-  (save-excursion
-    (goto-char (point-min))
-    (replace-regexp "ß" "&szlig;")))
+  (replace-in-buffer "ä" "&auml;")
+  (replace-in-buffer "ö" "&ouml;")
+  (replace-in-buffer "ü" "&uuml;")
+  (replace-in-buffer "ß" "&szlig;"))
 ;;; 901-snippets-activator.el ends here
